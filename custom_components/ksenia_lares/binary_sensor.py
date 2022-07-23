c517a0e624ded4e1535922b25b7002c812b9fda5
Francesco Gianni <francesco.gianni@live.it>
2022-07-23 11:53:24 +0200

    Update binary_sensor.py

--- custom_components/ksenia_lares/binary_sensor.py
+++ custom_components/ksenia_lares/binary_sensor.py
@@ -183,7 +183,7 @@ class LaresPartition(CoordinatorEntity, BinarySensorEntity):
     @property
     def is_on(self):
         """Return true if the binary sensor is on."""
-        return self._coordinator.data[self._idx] == "ARMED"
+        return self._coordinator.data[self._idx] == "ARMED" || self._coordinator.data[self._idx] == "ARMED_IMMEDIATE"
 
     @property
     def unique_id(self):
