50515969db550e66d0a1b9cf295af56c8740c6b1
Francesco Gianni <francesco.gianni@prima.it>
2022-12-12 22:26:21 +0100

    naming

--- custom_components/ksenia_lares/binary_sensor.py
+++ custom_components/ksenia_lares/binary_sensor.py
@@ -101,7 +101,7 @@ async def async_setup_entry(hass, config_entry, async_add_devices):
         return True
 
     async_add_devices(
-        LaresPartition(coordinator_partitions, f"lares.partition.{idx}", f"lares.partition.{partitions_descriptions[idx]}")
+        LaresPartition(coordinator_partitions, f"lares.partition.{idx}", partitions_descriptions[idx])
         for idx, zone in
         filter(filter_active_partition, enumerate(coordinator_partitions.data))
     )
@@ -122,7 +122,7 @@ async def async_setup_entry(hass, config_entry, async_add_devices):
         return output[1]["type"] != ZONE_STATUS_NOT_USED
 
     async_add_devices(
-        LaresOutput(coordinator_outputs, idx, f"lares.output.{outputs_descriptions[idx]}")
+        LaresOutput(coordinator_outputs, idx, outputs_descriptions[idx])
         for idx, out in filter(filter_active_output, enumerate(coordinator_outputs.data))
     )
 
@@ -141,7 +141,7 @@ class LaresSensor(CoordinatorEntity, BinarySensorEntity):
     @property
     def unique_id(self):
         """Return Unique ID string."""
-        return f"lares.zone.{self._idx}"
+        return self._idx
 
     @property
     def name(self):
@@ -188,7 +188,7 @@ class LaresPartition(CoordinatorEntity, BinarySensorEntity):
     @property
     def unique_id(self):
         """Return Unique ID string."""
-        return f"lares.partition.{self._idx}"
+        return self._idx
 
     @property
     def name(self):
@@ -218,7 +218,7 @@ class LaresOutput(CoordinatorEntity, BinarySensorEntity):
     @property
     def unique_id(self):
         """Return Unique ID string."""
-        return f"lares.output.{self._idx}"
+        return self._idx
 
     @property
     def name(self):
